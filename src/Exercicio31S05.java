9cee46c87bb165dc7a7f476e33d353348f8420db
Eurico Neto <eurico.neto6@gmail.com>
2024-10-09 11:27:14 -0300

    resolução E31S05

---
+++ src/Exercicio31S05.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Exercicio31S05 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double nota = 0;
+        double soma = 0;
+        double media = 0;
+        int contador = 0;
+
+
+
+        while (true) {
+            System.out.println("\ninforme a nota do aluno: ");
+            nota = sc.nextDouble();
+
+            if (nota < 0 || nota > 10) {
+                System.out.println("NOTA INVÁLIDA!");
+            } else {
+                soma += nota;   
+                contador++;  
+            }
+
+            if (contador == 2) {
+                media = soma / contador;
+
+                System.out.println("MÉDIA = " + media);
+
+                break;
+            }
+
+            
+            
+           
+        }
+
+            
+
+            
+
+        
+    }
+
+}
